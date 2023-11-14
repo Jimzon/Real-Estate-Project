6da13d02d1361195654f4572e43bfe5ee8ec51e8
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-11-14 14:55:13 +0800

    mongodb connect

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 
+mongoose.connect(
+  "mongodb+srv://jimzontrangia:luffyboy2022@mern-real-estate.qornwff.mongodb.net/mern-real-estate?retryWrites=true&w=majority"
+);
+
 const app = express();
 
 app.listen(3000, () => {
