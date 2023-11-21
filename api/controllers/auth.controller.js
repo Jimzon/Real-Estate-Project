33c9fd2388c5aef45e3344f8c8355a6320cce67f
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-11-21 17:53:14 +0800

    added cookies

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -22,5 +22,10 @@ export const signin = async (req, res, next) => {
     if (!validUser) return next(errorHandler(404, "User not found!"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(validUser);
   } catch (error) {}
 };
